77658d212860c64f2a5d7a7df49ede63f95d44bf
Kanani Het <tech.kananihet@gmail.com>
2026-02-19 13:30:43 +0530

    if else statement code done

---
+++ 01-Java/2nd Lacture/Conditional.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Conditional {
+
+    public static void main(String[] args) {
+
+        int salary = 23500;
+
+        if(salary > 10000) {
+            System.out.println(salary+=2000);
+        } else {
+            System.out.println(salary+=1000);
+        }
+    }
+}
