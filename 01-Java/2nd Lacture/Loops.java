6e4daa62164ee6777b6e76a417c6fff5c85e1a5d
Kanani Het <tech.kananihet@gmail.com>
2026-02-19 23:10:24 +0530

    Loops done

---
+++ 01-Java/2nd Lacture/Loops.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Loops {
+    public static void main(String[] args) {
+        // print 1 to 5;
+
+        // for(int num=1; num<=5; num++) {
+        //     System.out.println(num);
+        // }
+
+        // int num = 1;
+        // while(num <= 5) {
+        //     System.out.println(num);
+        //     num++;
+        // }
+
+        // int num = 1;
+        // do {
+        //     System.out.println(num);
+        //     num++;
+        // } while(num <= 5);
+    }
+    
+}
