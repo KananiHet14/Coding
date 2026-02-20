c3cc10625cdad5cff744baeb705d033b5086c5ec
Kanani Het <tech.kananihet@gmail.com>
2026-02-20 18:08:43 +0530

    2 questions done

---
+++ 01-Java/2nd Lacture/Questions2ndLacture.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Questions2ndLacture {
+
+    public static void main(String[] args) {
+        // question = find the largest of 3 numbers
+        // Scanner input = new Scanner(System.in);
+        // int a = input.nextInt();
+        // int b = input.nextInt();
+        // int c = input.nextInt();
+
+        // int max = a;
+
+        // if(b>max) {
+        //     max = b;
+        // }
+
+        // if(c>max) {
+        //     max = c;
+        // }
+
+        // System.out.println("largest of 3 number is : " + max);
+
+
+        // quyestion = case cheking of character upper case of lower case
+        // Scanner input = new Scanner(System.in);
+        // char ch = input.next().trim().charAt(0);
+
+        // if(ch>='a' && ch<='z') {
+        //     System.out.println("Lower case");
+        // } else {
+        //     System.out.println("Upper case");
+        // }
+
+    
+
+    }
+}
