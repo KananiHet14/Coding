065c405f7dd6d6388db39ab83bff18d67dc4aafa
Kanani Het <tech.kananihet@gmail.com>
2026-02-21 17:44:55 +0530

    Switch case program of input 1 to 7 and print day name based on number

---
+++ 01-Java/3rd Lacture/SwitchCase.java
@@ -0,0 +1,32 @@
+// import java.util.*;
+
+// public class SwitchCase {
+
+//     public static void main(String[] args) {
+        
+//         Scanner input = new Scanner(System.in);
+//         String fruit = input.next();
+
+//         switch(fruit) {
+//             case "mango" : System.out.println("king of fruits");
+//                             break;
+//             case "apple" : System.out.println("sweet red fruit");
+//                             break;
+//             case "orange" : System.out.println("citrus fruit");
+//                             break;
+//             default : System.out.println("please enter a valid fruit");
+
+//         int day = input.nextInt();
+//         switch(day) {
+//             case 1 -> System.out.println("Monday");
+//             case 2 -> System.out.println("Tuesday");
+//             case 3 -> System.out.println("Wednesday");
+//             case 4 -> System.out.println("Thursday");
+//             case 5 -> System.out.println("Friday");
+//             case 6 -> System.out.println("Saturday");
+//             case 7 -> System.out.println("Sunday");
+//             default -> System.out.println("please enter a valid day number");
+//         } 
+//     }
+// }
+// }
