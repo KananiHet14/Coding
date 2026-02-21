6bb7e639b52888c3584a122761b119cf8ee11c7b
Kanani Het <tech.kananihet@gmail.com>
2026-02-21 23:33:32 +0530

    3rd lacture done

---
+++ 01-Java/3rd Lacture/nestedcase.java
@@ -0,0 +1,24 @@
+// import java.util.*;
+
+// public class nestedcase {
+
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         int ID = input.nextInt();
+//         String department = input.nextLine();
+
+//         switch(ID) {
+//             case 1 -> System.out.println("Kanani Het");
+//             case 2 -> System.out.println("CSE");
+//             case 3 ->{ System.out.println("ECE");
+//                 switch(department) {
+//                     case "IT" -> System.out.println("Information Technology");
+//                     case "CSE" -> System.out.println("Computer Science and Engineering");
+//                     case "ECE" -> System.out.println("Electronics and Communication Engineering");
+//                     default -> System.out.println("please enter a valid department name");
+//                 }
+//                 }
+//             default -> System.out.println("please enter a valid ID");
+//         }
+//     }
+// }
