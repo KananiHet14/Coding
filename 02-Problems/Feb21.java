5d1a03cf2e989117704721ea1d77dc1f2301b76c
Kanani Het <tech.kananihet@gmail.com>
2026-02-22 13:02:01 +0530

    Number Classification System code done

---
+++ 02-Problems/Feb21.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+public class Feb21 {
+    public static void main(String[] args) {
+        // Write a Java program to find the sum of the first 10 natural numbers.
+        // Scanner input = new Scanner(System.in);
+        // int in = input.nextInt();
+        // int sum = 0;
+        // for(int i=1; i<=in; i++) {
+        //     sum = sum+i;
+        // }
+        // System.out.println("Sum of first " + in + " natural numbers is: " + sum);
+
+
+        
+        /*
+        Question = Write a program that takes a number as input and classifies it based on multiple criteria:
+        Requirements:
+        Check if the number is positive, negative, or zero
+        If positive:
+        Check if it's even or odd
+        Check if it's a single digit (1-9), double digit (10-99), or triple digit (100-999)
+        If negative:
+        Convert to positive and check if it's divisible by 5
+        If zero: Print "Zero has no sign"
+        */ 
+        // Scanner input = new Scanner(System.in);
+        // int number = input.nextInt();
+
+        // // positive 
+        // if(number > 0) {
+        //     System.out.println("youe entered positive number");
+        //     if(number % 2 == 0) {
+        //         System.out.println("youe entered even number");
+        //     } else {
+        //         System.out.println("youe entered odd number");
+        //     }
+
+        //     if(number < 10) {
+        //         System.out.println("youe entered single digit number");
+        //     } else if(number < 100) {
+        //         System.out.println("youe entered two digit number");
+        //     } else if(number < 1000) {
+        //         System.out.println("youe entered three digit number");
+        //     } else {
+        //         System.out.println("youe entered more digit number");
+        //     }
+        // } 
+        // // negative
+        // else if(number < 0) {
+        //     System.out.println("youe entered negative number");
+        //     number = Math.abs(number); // Convert to positive
+        //     System.out.println("after converting to positive: " + number);
+        //         if(number % 5 == 0) {
+        //             System.out.println("youe entered number is divisible by 5");
+        //         } else {
+        //             System.out.println("youe entered number is not divisible by 5");
+        //         }
+        // } 
+        //  // zero
+        // else {
+        //     System.out.println("youe entered zero");
+        //     System.out.println("zero has no sign");
+        // }
+
+
+
+
+        
+    }
+}
