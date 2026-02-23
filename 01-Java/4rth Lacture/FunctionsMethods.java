e0cc918639ffb11189749de01b57ba9ff2d83a72
Kanani Het <tech.kananihet@gmail.com>
2026-02-23 12:07:11 +0530

    first greeting function make

---
+++ 01-Java/4rth Lacture/FunctionsMethods.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class FunctionsMethods {
+    static void greeting() {
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        greeting();
+    }
+}
