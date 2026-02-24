b985367d07ec5fd5e40fa87a82511e5fb16c391b
Kanani Het <tech.kananihet@gmail.com>
2026-02-25 00:51:57 +0530

    3 questions done

---
+++ 02-Problems/Basic25feb.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Basic25feb {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        // Question 1 = even or odd
+        // int a = input.nextInt();
+        // if(a % 2 == 0) {
+        //     System.out.println("even");
+        // } else {
+        //     System.out.println("odd");
+        // }
+
+
+        // Question 2 = positive negatiive or zero
+        // int number = input.nextInt();
+
+        // if(number > 0) {
+        //     System.out.println("positive");
+        // } else if(number < 0) {
+        //     System.out.println("negative");
+        // } else {
+        //     System.out.println("zero");
+        // }
+
+
+        // Question 3 = Largest of two numbers
+        // int number1 = input.nextInt();
+        // int number2 = input.nextInt();
+
+        // if(number1 > number2) {
+        //     System.out.println("largest is " + number1);
+        // } else if(number2 > number1) {
+        //     System.out.println("largest is " + number2);
+        // } else {
+        //     System.out.println("both are equal");
+        // }
+
+    }
+}
