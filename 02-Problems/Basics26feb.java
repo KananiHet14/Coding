4053c258e5c6debd947a63f5808f94a8ae3458ad
Kanani Het <tech.kananihet@gmail.com>
2026-02-27 00:10:20 +0530

    normal again mean deleted for video

--- 02-Problems/Basics26feb.java
+++ 02-Problems/Basics26feb.java
@@ -4,51 +4,27 @@ public class Basics26feb {
     public static void main(String[] args) {
         // level 3 
         // Question 1 = Grade calculator
-        // Scanner input = new Scanner(System.in);
-        // System.out.print("Enter your marks :");
-        // int marks = input.nextInt();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your marks :");
+        int marks = input.nextInt();
 
-        // if(marks >=90 && marks <= 100) {
-        //     System.out.println("A+ GRADE");
-        // } else if(marks >= 75 && marks < 90) {
-        //     System.out.println("A GRADE");
+        if(marks >=90 && marks <= 100) {
+            System.out.println("A+ GRADE");
+        } else if(marks >= 75 && marks < 90) {
+            System.out.println("A GRADE");
 
-        // } else if(marks >= 60 && marks < 75) {
-        //     System.out.println("B GRADE");
+        } else if(marks >= 60 && marks < 75) {
+            System.out.println("B GRADE");
 
-        // } else if(marks >= 50 && marks < 60) {
-        //     System.out.println("C GRADE");
+        } else if(marks >= 50 && marks < 60) {
+            System.out.println("C GRADE");
 
-        // } else if(marks >= 40 && marks < 50) {
-        //     System.out.println("D GRADE");
+        } else if(marks >= 40 && marks < 50) {
+            System.out.println("D GRADE");
 
-        // } else {
-        //     System.out.println("FAIL");
+        } else {
+            System.out.println("FAIL");
 
-        // }
-
-        int n = 5;
-        int m = 5;
-
-        for(int i=n; i>=1; i--) {
-            for(int j=0; j<=m; j++) {
-                if(i==0 || j==0 || i==n || j==m) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-
-            }
-            System.out.println();
         }
-
-        for(int i=0; i<=n; i++) {
-            for(int j=0; j<=i; j++) {
-                System.out.print(j);
-
-            }
-            System.out.println();
-        }
-
     }
 }
